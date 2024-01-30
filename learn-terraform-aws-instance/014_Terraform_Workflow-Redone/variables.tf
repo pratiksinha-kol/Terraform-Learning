712a50fbc49bdb57c345c177335a6edf830c5cc6
Pratik Sinha <pratik.proriters@gmail.com>
2024-01-30 23:48:33 +0530

    Rectified variables.tf file

--- learn-terraform-aws-instance/014_Terraform_Workflow-Redone/variables.tf
+++ learn-terraform-aws-instance/014_Terraform_Workflow-Redone/variables.tf
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-
-
-variable "ami" {
-  type        = string
-  description = "AMI for your EC2"
-}
-
-variable "instance_type" {
-  type        = string
-  description = "Type of your instance"
-}
-
-variable "vpc_id" {
-  type        = string
-  description = "VPC ID where you want to launch your EC2 Instance"
-=======
 variable "ami" {
   type    = string
   description = "AMI for your EC2 INSTANCE"
@@ -28,16 +11,11 @@ variable "instance_type" {
 variable "vpc_id" {
   type = string
   description = "Fetch your VPC ID fro AWS Console"
->>>>>>> 1b4e078719d3ba82686fe39fab66864c70daf70c
 }
 
 variable "my_ip_cidr_ipv4" {
   type        = string
-<<<<<<< HEAD
   description = "Your own IP address in the format xx.xx.xx.xx/32"
-}
-=======
-  description = "PROVIDE YOUR IP i.e. xx.xx.xx.xx/32"
 }
 
 variable "bucket" {
@@ -45,4 +23,3 @@ variable "bucket" {
   description = "Terraform S3 bucket name"
   default = "terraform-bucket-for-workflow-example"
 }
->>>>>>> 1b4e078719d3ba82686fe39fab66864c70daf70c
