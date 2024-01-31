346ad149882642b59d0efe23f3993f72ca94b951
Pratik Sinha <pratik.proriters@gmail.com>
2024-01-31 23:51:43 +0530

    Modified modules section, Use subnet specified for the VPC

--- learn-terraform-aws-instance/013_Terraform_Modules/main.tf
+++ learn-terraform-aws-instance/013_Terraform_Modules/main.tf
@@ -3,10 +3,10 @@ terraform {
 }
 
 module "apache_module_example" {
-  source        = ".//terraform-aws-module-apache-example"
-  ami           = "ami-0c0b74d29acd0cd97"
-  instance_type = "t2.micro"
-  vpc_id        = "vpc-04560e2c74ba4b195"
+  source          = ".//terraform-aws-module-apache-example"
+  ami             = "ami-0c0b74d29acd0cd97"
+  instance_type   = "t2.micro"
+  vpc_id          = "vpc-04560e2c74ba4b195"
   my_ip_cidr_ipv4 = "115.187.62.117/32"
 }
 
